8157140865476309cd42af64afe2bdf7d9a6d16d
Mr-Sithel <6982111+Mr-Sithel@users.noreply.github.com>
2025-03-07 23:55:25 -0600

    Update PLD.lua

--- LuAshitacast-Profiles/config/addons/luashitacast/PLD.lua
+++ LuAshitacast-Profiles/config/addons/luashitacast/PLD.lua
@@ -22,7 +22,7 @@ local sets = {
         Legs = 'Blood Cuisses',
         Feet = 'Glt. Leggings +1',
     },       
-    Tp_Default = { 
+    Tp_Default = {
         Head = 'Homam Zucchetto',
         Neck = 'Fortitude Torque',
         Ear1 = 'Ethereal Earring',
@@ -34,18 +34,18 @@ local sets = {
         Back = 'Amemet Mantle +1',
         Waist = 'Swift Belt',
         Legs = 'Homam Cosciales',
-        Feet = 'Homam Gambieras',        
+        Feet = 'Homam Gambieras',
     },
-	Tp_HighAcc = {                        
-    },    
-	Town = { 		
+    Tp_HighAcc = {
     },
-	MP = {			
-	},
-	Resting = { 		
+    Town = {
     },
-	Precast = { 
-		Head = 'Koenig Schaller',
+    MP = {
+    },
+    Resting = {
+    },
+    Precast = {
+        Head = 'Koenig Schaller',
         Neck = 'Shield Torque',
         Ear1 = 'Ethereal Earring',
         Ear2 = 'Cassie Earring',
@@ -57,7 +57,7 @@ local sets = {
         Waist = 'Steppe Sash',
         Legs = 'Blood Cuisses',
         Feet = 'Glt. Leggings +1',
-	},
+    },
     -- Haste Gear for Utusemi: Ichi or Ni
     Utsu = {
         Head = 'Homam Zucchetto',
@@ -71,25 +71,11 @@ local sets = {
         Back = 'Valor Cape',
         Waist = 'Swift Belt',
         Legs = 'Homam Cosciales',
-        Feet = 'Homam Gambieras',        
-	},
+        Feet = 'Homam Gambieras',
+    },
     -- Enimity gear for Hate
-    Flash = { 
+    Flash = {
         Head = 'Aegishjalmr',
-        Neck = 'Harmonia\'s Torque',
-        Ear1 = 'Ethereal Earring',
-        Ear2 = 'Cassie Earring',
-        Body = 'Valor Surcoat',
-        Hands = 'Hydra Moufles',
-        Ring1 = 'Bomb Queen Ring',
-        Ring2 = 'Hercules\' Ring',
-        Back = 'Valor Cape',
-        Waist = 'Warwolf Belt',
-        Legs = 'Hydra Brayettes',
-        Feet = 'Valor Leggings',       
-	},
-    Cure = { 
-		Head = 'Aegishjalmr',
         Neck = 'Harmonia\'s Torque',
         Ear1 = 'Ethereal Earring',
         Ear2 = 'Cassie Earring',
@@ -101,8 +87,22 @@ local sets = {
         Waist = 'Warwolf Belt',
         Legs = 'Hydra Brayettes',
         Feet = 'Valor Leggings',
-	},
-	Haste = {
+    },
+    Cure = {
+        Head = 'Aegishjalmr',
+        Neck = 'Harmonia\'s Torque',
+        Ear1 = 'Ethereal Earring',
+        Ear2 = 'Cassie Earring',
+        Body = 'Valor Surcoat',
+        Hands = 'Hydra Moufles',
+        Ring1 = 'Bomb Queen Ring',
+        Ring2 = 'Hercules\' Ring',
+        Back = 'Valor Cape',
+        Waist = 'Warwolf Belt',
+        Legs = 'Hydra Brayettes',
+        Feet = 'Valor Leggings',
+    },
+    Haste = {
         Head = 'Homam Zucchetto',
         Neck = 'Harmonia\'s Torque',
         Ear1 = 'Ethereal Earring',
@@ -114,9 +114,9 @@ local sets = {
         Back = 'Valor Cape',
         Waist = 'Swift Belt',
         Legs = 'Homam Cosciales',
-        Feet = 'Homam Gambieras',        
-	},
-	MDT = { 
+        Feet = 'Homam Gambieras',
+    },
+    MDT = {
         Head = 'Koenig Schaller',
         Neck = 'Shield Torque',
         Ear1 = 'Merman\'s Earring',
@@ -128,9 +128,9 @@ local sets = {
         Back = 'Valor Cape',
         Waist = 'Steppe Sash',
         Legs = 'Blood Cuisses',
-        Feet = 'Glt. Leggings +1',               
-	},
-	PDT = { 
+        Feet = 'Glt. Leggings +1',
+    },
+    PDT = {
         Head = 'Koenig Schaller',
         Neck = 'Shield Torque',
         Ear1 = 'Ethereal Earring',
@@ -142,10 +142,10 @@ local sets = {
         Back = 'Valor Cape',
         Waist = 'Steppe Sash',
         Legs = 'Blood Cuisses',
-        Feet = 'Glt. Leggings +1',       	
-	},	
-	-- Enimity gear for provoke
-	Provoke = { 
+        Feet = 'Glt. Leggings +1',
+    },
+    -- Enimity gear for provoke
+    Provoke = {
         Head = 'Aegishjalmr',
         Neck = 'Harmonia\'s Torque',
         Ear1 = 'Ethereal Earring',
@@ -157,20 +157,20 @@ local sets = {
         Back = 'Valor Cape',
         Waist = 'Warwolf Belt',
         Legs = 'Hydra Brayettes',
-        Feet = 'Valor Leggings',                              		
-	},
-    Cover = {  
+        Feet = 'Valor Leggings',
+    },
+    Cover = {
         Head = 'Gallant Coronet',
-		Body = 'Valor Surcoat',                             		
-	},
-    Rampart = {  
-        Head = 'Valor Coronet',                             		
-	},
-    Sentinel = { 
-        Feet = 'Valor Leggings',                              		
-	},
-    Shield_Bash = {                               		
-	},
+        Body = 'Valor Surcoat',
+    },
+    Rampart = {
+        Head = 'Valor Coronet',
+    },
+    Sentinel = {
+        Feet = 'Valor Leggings',
+    },
+    Shield_Bash = {
+    },
     Ws_Default = {
         Head = 'Voyager Sallet',
         Neck = 'Spike Necklace',
@@ -184,7 +184,7 @@ local sets = {
         Waist = 'Warwolf Belt',
         Legs = 'Valor Breeches',
         Feet = 'Hct. Leggings',
-    },    
+    },
 };
 profile.Sets = sets;
 
@@ -199,65 +199,65 @@ end
 
 profile.OnLoad = function()
     gSettings.AllowAddSet = true;
-	gcdisplay.CreateToggle('(F10) MDT', false);
-	gcdisplay.CreateToggle('(F12) PDT', false);
-	gcdisplay.CreateToggle('(F9) MP', false);    
-	gcdisplay.CreateCycle('/pld tp ', { [1] = 'Default', [2] = 'HighAcc' }); --/pld tp       
-	AshitaCore:GetChatManager():QueueCommand(-1, '/bind F9 /pld mptoggle');
-	AshitaCore:GetChatManager():QueueCommand(-1, '/bind F10 /pld mdttoggle');
-	AshitaCore:GetChatManager():QueueCommand(-1, '/bind F12 /pld pdttoggle');
-	AshitaCore:GetChatManager():QueueCommand(-1, '/alias /pld /lac fwd'); 
-	AshitaCore:GetChatManager():QueueCommand(1, '/macro book 4');
+    gcdisplay.CreateToggle('(F10) MDT', false);
+    gcdisplay.CreateToggle('(F12) PDT', false);
+    gcdisplay.CreateToggle('(F9) MP', false);
+    gcdisplay.CreateCycle('/pld tp ', { [1] = 'Default', [2] = 'HighAcc' }); --/pld tp
+    AshitaCore:GetChatManager():QueueCommand(-1, '/bind F9 /pld mptoggle');
+    AshitaCore:GetChatManager():QueueCommand(-1, '/bind F10 /pld mdttoggle');
+    AshitaCore:GetChatManager():QueueCommand(-1, '/bind F12 /pld pdttoggle');
+    AshitaCore:GetChatManager():QueueCommand(-1, '/alias /pld /lac fwd'); 
+    AshitaCore:GetChatManager():QueueCommand(1, '/macro book 4');
     AshitaCore:GetChatManager():QueueCommand(1, '/macro set 1');	
-	gcdisplay.Initialize();
-	gcinclude.SetAlias();
+    gcdisplay.Initialize();
+    gcinclude.SetAlias();
     profile.SetLockStyle();
 end
 
 profile.OnUnload = function()
-	AshitaCore:GetChatManager():QueueCommand(-1, '/unbind F9');
-	AshitaCore:GetChatManager():QueueCommand(-1, '/unbind F10');
-	AshitaCore:GetChatManager():QueueCommand(-1, '/unbind F12');
-	AshitaCore:GetChatManager():QueueCommand(-1, '/alias delete /pld');
-	gcdisplay.Destroy();
-	gcinclude.ClearAlias();	
+    AshitaCore:GetChatManager():QueueCommand(-1, '/unbind F9');
+    AshitaCore:GetChatManager():QueueCommand(-1, '/unbind F10');
+    AshitaCore:GetChatManager():QueueCommand(-1, '/unbind F12');
+    AshitaCore:GetChatManager():QueueCommand(-1, '/alias delete /pld');
+    gcdisplay.Destroy();
+    gcinclude.ClearAlias();
 end
 
 profile.HandleCommand = function(args)
-    gcinclude.HandleCommands(args);    
+    gcinclude.HandleCommands(args);
     
-	if (args[1] == 'tp') then    
-		gcdisplay.AdvanceCycle('/pld tp ');
-	end    
-    if (args[1] == 'mdttoggle') then
-		gcdisplay.AdvanceToggle('(F10) MDT');
-	end
-	if (args[1] == 'pdttoggle') then
-		gcdisplay.AdvanceToggle('(F12) PDT');
+    if (args[1] == 'tp') then
+        gcdisplay.AdvanceCycle('/pld tp ');
     end
-	if (args[1] == 'mptoggle') then
-		gcdisplay.AdvanceToggle('(F9) MP');
+    if (args[1] == 'mdttoggle') then
+        gcdisplay.AdvanceToggle('(F10) MDT');
+    end
+    if (args[1] == 'pdttoggle') then
+        gcdisplay.AdvanceToggle('(F12) PDT');
+    end
+    if (args[1] == 'mptoggle') then
+        gcdisplay.AdvanceToggle('(F9) MP');
     end
 end
 
-profile.HandleDefault = function() 	
-    local player = gData.GetPlayer(); 
+profile.HandleDefault = function()
+    local player = gData.GetPlayer();
 
-    EquipHpBasedGear(player);   
+    EquipHpBasedGear(player);
     if (player.Status == 'Idle') then
-        gFunc.EquipSet(sets.Idle_Default);       
-    elseif (player.Status == 'Engaged') then		
-		gFunc.EquipSet('TP_' .. gcdisplay.GetCycle('/pld tp '));
-	elseif (player.Status == 'Resting') then
-        gFunc.EquipSet(sets.Resting);		
-	end
-    gFunc.EquipSet(gcinclude.BuildLockableSet(gData.GetEquipment())) 
+        gFunc.EquipSet(sets.Idle_Default);
+    elseif (player.Status == 'Engaged') then
+        gFunc.EquipSet('TP_' .. gcdisplay.GetCycle('/pld tp '));
+    elseif (player.Status == 'Resting') then
+        gFunc.EquipSet(sets.Resting);
+    end
+    gFunc.EquipSet(gcinclude.BuildLockableSet(gData.GetEquipment()))
 
-	if (gcdisplay.GetToggle('(F10) MDT') == true) then gFunc.EquipSet(sets.MDT) end	
-	if (gcdisplay.GetToggle('(F12) PDT') == true) then gFunc.EquipSet(sets.PDT) end
-	if (gcdisplay.GetToggle('(F9) MP') == true) then gFunc.EquipSet(sets.MP) end	
-	
-	gcinclude.TownGear();
+    if (gcdisplay.GetToggle('(F10) MDT') == true) then gFunc.EquipSet(sets.MDT) end
+    if (gcdisplay.GetToggle('(F12) PDT') == true) then gFunc.EquipSet(sets.PDT) end
+    if (gcdisplay.GetToggle('(F9) MP') == true) then gFunc.EquipSet(sets.MP) end
+        
+    gcinclude.TownGear();
     gcinclude.CheckCommonDebuffs();
 
     local subjob = AshitaCore:GetMemoryManager():GetPlayer():GetSubJob();
@@ -285,36 +285,36 @@ profile.SetLockStyle = function ()
 end
 
 profile.HandlePrecast = function()
-    local spell = gData.GetAction();    
+    local spell = gData.GetAction();
     
     gFunc.EquipSet(sets.Precast);
-	
-	if (gcdisplay.GetToggle('(F10) MDT') == true) then gFunc.EquipSet(sets.MDT) end	
-	if (gcdisplay.GetToggle('(F12) PDT') == true) then gFunc.EquipSet(sets.PDT) end
-	if (gcdisplay.GetToggle('(F9) MP') == true) then gFunc.EquipSet(sets.MP) end    
+        
+    if (gcdisplay.GetToggle('(F10) MDT') == true) then gFunc.EquipSet(sets.MDT) end
+    if (gcdisplay.GetToggle('(F12) PDT') == true) then gFunc.EquipSet(sets.PDT) end
+    if (gcdisplay.GetToggle('(F9) MP') == true) then gFunc.EquipSet(sets.MP) end
 end
 
 profile.HandleMidcast = function()
     local target = gData.GetActionTarget();
-    local spell = gData.GetAction();   
+    local spell = gData.GetAction();
     local action = gData.GetAction();
-	if (spell.Skill == 'Enhancing Magic') then	
-		gFunc.EquipSet(sets.Haste);
+    if (spell.Skill == 'Enhancing Magic') then
+        gFunc.EquipSet(sets.Haste);
     elseif (action.Skill == 'Ninjutsu') then
-        gFunc.EquipSet(sets.Utsu);        
+        gFunc.EquipSet(sets.Utsu);
     elseif (action.Name == 'Flash') then
         gFunc.EquipSet(sets.Flash);
     elseif (action.Skill == 'Healing Magic') then
             gFunc.EquipSet(sets.Cure);
     end 
 
-	if (gcdisplay.GetToggle('(F10) MDT') == true) then gFunc.EquipSet(sets.MDT) end	
-	if (gcdisplay.GetToggle('(F12) PDT') == true) then gFunc.EquipSet(sets.PDT) end
-	if (gcdisplay.GetToggle('(F9) MP') == true) then gFunc.EquipSet(sets.MP) end
+    if (gcdisplay.GetToggle('(F10) MDT') == true) then gFunc.EquipSet(sets.MDT) end	
+    if (gcdisplay.GetToggle('(F12) PDT') == true) then gFunc.EquipSet(sets.PDT) end
+    if (gcdisplay.GetToggle('(F9) MP') == true) then gFunc.EquipSet(sets.MP) end
 end
 
 profile.HandleItem = function()
-    gcinclude.DoItem(); 
+    gcinclude.DoItem();
 end
 
 profile.HandlePreshot = function()
@@ -329,13 +329,13 @@ profile.HandleWeaponskill = function()
     local canWS = gcinclude.CheckWsBailout();
         if (canWS == false) then gFunc.CancelAction() return;
         else
-            local action = gData.GetAction();    
-            gFunc.EquipSet(sets.Ws_Default);        
+            local action = gData.GetAction();
+            gFunc.EquipSet(sets.Ws_Default);
     end
-	
-	if (gcdisplay.GetToggle('(F10) MDT') == true) then gFunc.EquipSet(sets.MDT) end	
-	if (gcdisplay.GetToggle('(F12) PDT') == true) then gFunc.EquipSet(sets.PDT) end
-	if (gcdisplay.GetToggle('(F9) MP') == true) then gFunc.EquipSet(sets.MP) end
+        
+    if (gcdisplay.GetToggle('(F10) MDT') == true) then gFunc.EquipSet(sets.MDT) end	
+    if (gcdisplay.GetToggle('(F12) PDT') == true) then gFunc.EquipSet(sets.PDT) end
+    if (gcdisplay.GetToggle('(F9) MP') == true) then gFunc.EquipSet(sets.MP) end
 end
 
 profile.HandleAbility = function()
@@ -345,7 +345,7 @@ profile.HandleAbility = function()
         gFunc.EquipSet(sets.Provoke)
     elseif (action.Name == 'Cover') then
         gFunc.EquipSet(sets.Cover)
-	elseif (action.Name == 'Rampart') then
+    elseif (action.Name == 'Rampart') then
         gFunc.EquipSet(sets.Rampart)
     elseif (action.Name == 'Sentinel') then
         gFunc.EquipSet(sets.Sentinel)

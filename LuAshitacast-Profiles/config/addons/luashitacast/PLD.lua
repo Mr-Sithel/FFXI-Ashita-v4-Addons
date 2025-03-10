e7c5f543ed7bb78518dd1060098be52d71a2e292
Mr-Sithel <6982111+Mr-Sithel@users.noreply.github.com>
2025-03-10 01:46:18 -0500

    Update PLD.lua

--- LuAshitacast-Profiles/config/addons/luashitacast/PLD.lua
+++ LuAshitacast-Profiles/config/addons/luashitacast/PLD.lua
@@ -9,6 +9,7 @@ gcinclude.VarTable();
 -- F9 Toggles locking a MP set
 -- F10 Toggles locking a MDT set
 -- F12 Toggles locking a PDT set
+-- Check below for /lockstyleset (4 is being used by default)
 
 local parade_gorget = true
 local hercules_ring = true
@@ -278,15 +279,15 @@ profile.SetLockStyle = function ()
     local player = gData.GetPlayer();
     if (player.SubJob == 'NIN') then
         AshitaCore:GetChatManager():QueueCommand(1, '/macro set 1');
-        --AshitaCore:GetChatManager():QueueCommand(1, '/sl others on');
+        --AshitaCore:GetChatManager():QueueCommand(1, '/sl others on');  -- Stylist Plugin Needed
         AshitaCore:GetChatManager():ExecuteScriptString("/wait 3;/lockstyleset 4", '',true)
     elseif (player.SubJob == 'WAR') then
         AshitaCore:GetChatManager():QueueCommand(1, '/macro set 1');
-        --AshitaCore:GetChatManager():QueueCommand(1, '/sl others on');
+        --AshitaCore:GetChatManager():QueueCommand(1, '/sl others on');  -- Stylist Plugin Needed
         AshitaCore:GetChatManager():ExecuteScriptString("/wait 3;/lockstyleset 4", '',true)
     elseif (player.SubJob == 'WHM') then
         AshitaCore:GetChatManager():QueueCommand(1, '/macro set 1');
-        --AshitaCore:GetChatManager():QueueCommand(1, '/sl others on');
+        --AshitaCore:GetChatManager():QueueCommand(1, '/sl others on');  -- Stylist Plugin Needed
         AshitaCore:GetChatManager():ExecuteScriptString("/wait 3;/lockstyleset 4", '',true)
     end    
 end

c124bfc4c1bceea5677b1c9f538e5293aa7a59ce
Mr-Sithel <6982111+Mr-Sithel@users.noreply.github.com>
2025-03-10 01:41:08 -0500

    Update BRD.lua

--- LuAshitacast-Profiles/config/addons/luashitacast/BRD.lua
+++ LuAshitacast-Profiles/config/addons/luashitacast/BRD.lua
@@ -3,6 +3,14 @@ gcinclude = gFunc.LoadFile('common\\gcinclude.lua');
 local gcdisplay = gFunc.LoadFile('common\\gcdisplay.lua');
 gcinclude.VarTable();
 
+-- Whatever Town Movement speed body(s) you have or don't, change lines 4-7 to True or False in the gcinclude.lua
+-- /wsdistance or /wsdistance ##  (to change ws cancel range)
+-- /brd tp to change Tp sets from Default/HighAcc
+-- F9 Toggles locking a HP set
+-- F10 Toggles locking a MDT set
+-- F12 Toggles locking a PDT set
+-- Check below for /lockstyleset (4 is being used by default)
+
 local minstrels_earring = false
 local minstrels_earring_slot = 'Ear2'
 
@@ -80,7 +88,6 @@ profile.OnLoad = function()
     AshitaCore:GetChatManager():QueueCommand(-1, '/alias /brd /lac fwd'); 
     AshitaCore:GetChatManager():QueueCommand(1, '/macro book 5');
     AshitaCore:GetChatManager():QueueCommand(1, '/macro set 1');
-    --AshitaCore:GetChatManager():QueueCommand(1, '/lockstyle on');
     gcdisplay.Initialize();
     gcinclude.SetAlias();
     profile.SetLockStyle();
@@ -145,15 +152,11 @@ profile.SetLockStyle = function ()
     local player = gData.GetPlayer();
     if (player.SubJob == 'NIN') then
         AshitaCore:GetChatManager():QueueCommand(1, '/macro set 1');
-        --AshitaCore:GetChatManager():QueueCommand(1, '/sl others on');
+        --AshitaCore:GetChatManager():QueueCommand(1, '/sl others on'); -- Stylist plugin is needed for this to work
         AshitaCore:GetChatManager():ExecuteScriptString("/wait 3;/lockstyleset 4", '',true)
-    elseif (player.SubJob == 'SAM') then
-        AshitaCore:GetChatManager():QueueCommand(1, '/macro set 2');
-        --AshitaCore:GetChatManager():QueueCommand(1, '/sl others on');
-        AshitaCore:GetChatManager():ExecuteScriptString("/wait 3;/lockstyleset 4", '',true)
-    elseif (player.SubJob == 'THF') then
-        AshitaCore:GetChatManager():QueueCommand(1, '/macro set 3');
-        --AshitaCore:GetChatManager():QueueCommand(1, '/sl others on');
+    elseif (player.SubJob == 'WHM') then
+        AshitaCore:GetChatManager():QueueCommand(1, '/macro set 1');
+        --AshitaCore:GetChatManager():QueueCommand(1, '/sl others on'); -- Stylist plugin is needed for this to work
         AshitaCore:GetChatManager():ExecuteScriptString("/wait 3;/lockstyleset 4", '',true)
     end
 end
